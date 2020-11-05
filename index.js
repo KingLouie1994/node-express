7e79f9fe9eeb482dbf5f1ab9ac6f880e22a3def7
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-11-05 13:22:42 +0100

    added styling to the html pages

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(shopRoutes);
 app.use("/", (req, res, next) => {
   res
     .status(404)
-    .sendFile(path.join(__dirname, "./", "views", "page-not-found.html"));
+    .sendFile(path.join(__dirname, ".", "views", "page-not-found.html"));
 });
 
 app.listen(3000);
