a66d0820252dec884db03d90c456b6217364f2cc
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-11-05 12:34:04 +0100

    added views with html files that are shown on the routes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const bodyParser = require("body-parser");
 
@@ -10,10 +11,12 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/admin", adminRoutes);
-app.use("/shop", shopRoutes);
+app.use(shopRoutes);
 
 app.use("/", (req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res
+    .status(404)
+    .sendFile(path.join(__dirname, "./", "views", "page-not-found.html"));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,12 @@
 const { Router } = require("express");
+const path = require("path");
 
 const express = require("express");
 
 const router = express.Router();
 
-router.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/admin/product' method='POST'><input type='text' name='title' /><button type='submit'>Add Product</button></form>"
-  );
+router.get("/add-product", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 router.post("/product", (req, res, next) => {
