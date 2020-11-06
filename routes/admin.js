fe17b23bbeff8b211136db6ef67eb2152736ddd0
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-11-06 10:41:34 +0100

    added products get pushed into products array now

--- routes/admin.js
+++ routes/admin.js
@@ -4,13 +4,18 @@ const express = require("express");
 
 const router = express.Router();
 
+const products = [];
+
+// /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   res.sendFile(path.join(__dirname, "..", "views", "add-product.html"));
 });
 
-router.post("/product", (req, res, next) => {
-  console.log(req.body);
+// /admin/add-product => POST
+router.post("/add-product", (req, res, next) => {
+  products.push({ title: req.body.title });
   res.redirect("/");
 });
 
-module.exports = router;
+exports.router = router;
+exports.products = products;
