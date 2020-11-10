211d9a83c9675d688b33b9c939a33ce005b9d990
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-11-10 13:20:39 +0100

    successfully connected to mongoDB

--- util/database.js
+++ util/database.js
@@ -1,8 +1,21 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", "Baumkastanien3", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://Node-Complete-Guide2020:Node-Complete-Guide2020@node-complete-guide.sj1wi.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }
+  )
+    .then((client) => {
+      console.log("Successfully connected to MongoDB");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize
+module.exports = mongoConnect;
