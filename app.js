746913c6a5c4f173374645d85b055acba0832b48
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-11-10 18:18:56 +0100

    the cart works

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use((req, res, next) => {
   User.findById("5faaae6a14577c73c352bb74")
     .then((user) => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => {
