7d67260d7b575762256b512d357bd901f0475138
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-11-10 10:11:07 +0100

    Deleting products via admin products page works

--- controllers/admin.js
+++ controllers/admin.js
@@ -19,7 +19,9 @@ exports.postAddProduct = (req, res, next) => {
     imageUrl: imageUrl,
     description: description,
   })
-    .then((result) => {})
+    .then((result) => {
+      res.redirect("/admin/products");
+    })
     .catch((err) => {
       console.log(err);
     });
@@ -84,6 +86,12 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect("/admin/products");
+  Product.findByPk(prodId)
+    .then((product) => {
+      return product.destroy();
+    })
+    .then((result) => {
+      res.redirect("/admin/products");
+    })
+    .catch((err) => console.log(err));
 };
