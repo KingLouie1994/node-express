e0fe1a604eba032c7b8a23a13aa92a06b4ee75ee
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-11-11 11:53:33 +0100

    set first cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,19 @@
 exports.getLogin = (req, res, next) => {
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login'
+  let isLoggedIn;
+  if (req.get("Cookie")) {
+    isLoggedIn = req.get("Cookie").split("=")[1].trim();
+  } else {
+    isLoggedIn = false;
+  }
+  console.log(isLoggedIn);
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    isAuthenticated: isLoggedIn,
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader("Set-Cookie", "loggedIn=true");
+  res.redirect("/");
 };
