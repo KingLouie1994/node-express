296bf3d19e51f5a9be34e8db7e09819c9aecc562
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-11-11 14:42:58 +0100

    users can signup and bcrypjs is installed to hash passwords

--- app.js
+++ app.js
@@ -60,18 +60,6 @@ mongoose
     useNewUrlParser: true,
   })
   .then((result) => {
-    User.findOne().then((user) => {
-      if (!user) {
-        const user = new User({
-          name: "Max",
-          email: "max@test.com",
-          cart: {
-            items: [],
-          },
-        });
-        user.save();
-      }
-    });
     app.listen(3000);
   })
   .catch((err) => {
